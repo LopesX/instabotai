241f3da821124a60ac7fdee35d9f2d45375e2487
Steffan Jensen <6hourapp@gmail.com>
2019-05-26 00:52:22 +0000

    Create reply_pending_inbox.py

---
+++ examples/reply_pending_inbox.py
@@ -0,0 +1,35 @@
+"""
+    instabot example
+    Workflow:
+        Reply to all pending messages in inbox.
+        Find all pending messages in inbox -> Accept messages -> reply to message.
+"""
+
+import argparse
+import os
+import sys
+import time
+
+sys.path.append(os.path.join(sys.path[0], '../'))
+from instabot import Bot
+
+parser = argparse.ArgumentParser(add_help=True)
+parser.add_argument('-u', type=str, help="username")
+parser.add_argument('-p', type=str, help="password")
+parser.add_argument('-proxy', type=str, help="proxy")
+args = parser.parse_args()
+
+
+bot = Bot()
+bot.login(username=args.u, password=args.p,
+          proxy=args.proxy)
+
+bot.api.get_pending_inbox()
+for w in bot.api.last_json["inbox"]["threads"]:
+    thread_id = w["thread_id"]
+    username = w["users"][0]["username"]
+    userid = bot.get_user_id_from_username(username)
+    print(thread_id)
+    bot.api.approve_pending_thread(thread_id)
+    time.sleep(20)
+    bot.send_message("Thanks please like all my pictures", userid, thread_id=thread_id)
