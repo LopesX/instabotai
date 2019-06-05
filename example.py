9c66a12b7ff00324fdc3de99e709ceb3fdd85742
Steffan Jensen <6hourapp@gmail.com>
2019-06-05 21:53:33 +0000

    more functions + story_upload works again

--- example.py
+++ example.py
@@ -24,10 +24,9 @@ import os
 import sys
 import json
 import time
-from tqdm import tqdm
 import logging
 from random import randint
-
+from tqdm import tqdm
 # Config
 image_comment = "Wow nice picture, i have just reposted it"
 
@@ -61,13 +60,17 @@ help_output()
 userlist = args.l
 instagramtags = args.t
 
+username = InstaUsername
+
+sys.path.append(os.path.join(sys.path[0], '../'))
+
+USERNAME_DATABASE = 'username_database.txt'
+POSTED_MEDIAS = 'posted_medias.txt'
+
 with open('instaprofiles.txt', 'w') as f:
     if f:
         userlist = userlist.replace(",", "\n")
     f.write(userlist)
-
-
-username = InstaUsername
 
 # Open Userdb and put them into a list also write your username to database
 def open_profiles():
@@ -93,14 +96,47 @@ def open_profiles():
 number_last_photos = 3
 x = 0
 
-sys.path.append(os.path.join(sys.path[0], '../'))
+
+def increment():
+    global x
+    x = x+1
 
 
-USERNAME_DATABASE = 'username_database.txt'
-POSTED_MEDIAS = 'posted_medias.txt'
+def bot_upload_photo(instapath, tags, media_id):
+    ''' Upload photo to instagram'''
+    bot.api.upload_photo(instapath, tags)
+    log.info("Reposted: " + media_id)
 
-# Instagram image scraper
+def bot_like(media_id):
+    ''' Like image on instagram '''
+    bot.api.like(media_id)
+    log.info("Liked media id: " + media_id)
+
+def bot_comment(media_id, image_comment):
+    ''' Comment image on instagram '''
+    bot.comment(media_id, image_comment)
+    log.info("Commented: " + media_id)
+
+def send_dm(scraped_user_id):
+    ''' send dm on instagram '''
+    bot.send_message("hi i just reposted your photo", scraped_user_id)
+    log.info("Private dm send to " + scraped_user_id)
+    log.info("Wait 2200 - 2600 sec for next repost")
+
+def file_creator(file_ending, encoding, write):
+    ''' File creator for log files '''
+    with open(username + file_ending, encoding) as f:
+        file_output = f.write(str(write))
+        return file_output
+
+def random_sleep(number1, number2):
+    ''' Random sleep between two numbers'''
+    time_sleep = time.sleep(randint(number1, number2))
+    return time_sleep
+
+
 def InstaImageScraper():
+    ''' Scrape image on profiles '''
     imgScraper = insta.InstagramScraper(usernames=[insta_profiles[x]],
                                         maximum=number_last_photos,
                                         media_metadata=True, latest=True,
@@ -108,11 +144,30 @@ def InstaImageScraper():
     imgScraper.scrape()
     print("image scraping is running, please wait 50 seconds.")
 
+def face_detection(path_to_image, new_media_id, media_id):
+    ''' Face Detection for image '''
+    image = face_recognition.load_image_file(path_to_image)
+    face_locations = face_recognition.face_locations(image)
+    # If no face located scrape the next profile
+    if not face_locations:
+        log.info("There is no Face Detected scraping next profile")
+        increment()
+        log.info(scraped_user)
+        random_sleep(1, 6)
+        instascraper(bot, new_media_id, path=POSTED_MEDIAS)
+    else:
+        log.info("There is a Face Detected scraping and posting this image")
+        log.info(scraped_user)
+        random_sleep(1, 2)
+        log.info("Media Id:" + str(media_id))
+        log.info("Face Location: " + str(face_locations))
+        log.info("Path to image: " + path_to_image)
+
 # Instagram manipulate image and repost them
 # While x is less than instaprofiles loop this
 def instascraper(bot, new_media_id, path=POSTED_MEDIAS):
     InstaImageScraper()
-    time.sleep(randint(1, 5))
+    random_sleep(1, 5)
     global x
     while x < len(insta_profiles):
         try:
@@ -128,40 +183,21 @@ def instascraper(bot, new_media_id, path=POSTED_MEDIAS):
                 # Output media id of image
                 media_id = (json_data["GraphImages"][0]["id"])
                 log.info("Found media id: " + media_id)
-                time.sleep(randint(5, 10))
+                random_sleep(1, 5)
                 logging.info("image string generated " + newstr)
                 time.sleep(randint(1, 5))
                 imgUrl = newstr.split('?')[0].split('/')[-1]
                 global instapath
                 instapath = insta_profiles[x] + '/' + imgUrl
                 logging.info("Found Instagram Path to Image: " + instapath)
-                time.sleep(randint(1, 5))
+                random_sleep(1, 5)
                 global tags
                 tags = "@" + insta_profiles[x] + " " + instagramtags
-                # Execute Face Detection
-                # Locate Face On image scraped
-                image = face_recognition.load_image_file(instapath)
-                face_locations = face_recognition.face_locations(image)
-                # If no face located scrape the next profile
-                if not face_locations:
-                    log.info("There is no Face Detected scraping next profile")
-                    x += 1
-                    log.info(scraped_user)
-                    time.sleep(randint(5, 10))
-                    instascraper(bot, new_media_id, path=POSTED_MEDIAS)
-                else:
-                    log.info("There is a Face Detected scraping and posting this image")
-                    log.info(scraped_user)
-                    time.sleep(randint(5, 10))
-                    log.info("Media Id:" + str(media_id))
-                    log.info("Face Location: " + str(face_locations))
-                    log.info("Path to image: " + instapath)
-
-
+                # Execute face_detection
+                face_detection(instapath, new_media_id, media_id)
                 # Append username info to csv file
                 try:
-                    with open(username + '.tsv', 'a+') as f:
-                        f.write(str(saveStats))
+                    file_creator('.tsv', 'a+', saveStats)
                     with open(username + '.tsv', 'r') as f:
                         last_line = f.readlines()[-2].replace("False", "")
                     log.info("Date - Time - Followers - Following - Posts")
@@ -169,8 +205,7 @@ def instascraper(bot, new_media_id, path=POSTED_MEDIAS):
 
                 # Write username tsv file if it does not exist
                 except:
-                    with open(username + '.tsv', 'w+') as f:
-                        f.write(str(saveStats))
+                    file_creator('.tsv', 'w+', saveStats)
                     with open(username + '.tsv', 'r') as f:
                         last_line = f.readlines()[-1]
                     log.info("Date - Time - Followers - Following - Posts")
@@ -178,8 +213,7 @@ def instascraper(bot, new_media_id, path=POSTED_MEDIAS):
 
                 # Append username info to csv file
                 try:
-                    with open(username + '_posted.tsv', 'a+') as f:
-                        f.write(str(imgUrl + "\n"))
+                    file_creator('_posted.tsv', 'a+', imgUrl + '\n')
                     with open(username + '_posted.tsv', 'r') as f:
                         last_line = f.readlines()[-1]
                     with open(username + '_posted.tsv', 'r') as f:
@@ -193,19 +227,18 @@ def instascraper(bot, new_media_id, path=POSTED_MEDIAS):
                             log.info("Image found in database scraping next profile")
                             x += 1
                             log.info("image found of: " + scraped_user)
-                            time.sleep(randint(5, 10))
+                            random_sleep(1, 2)
                             instascraper(bot, new_media_id, path=POSTED_MEDIAS)
 
                         except:
                             log.info("image found of: " + scraped_user)
                             x += 1
-                            time.sleep(randint(5, 20))
+                            random_sleep(1, 2)
                             instascraper(bot, new_media_id, path=POSTED_MEDIAS)
 
                 # Write username tsv file if it does not exist
                 except:
-                    with open(username + '_posted.tsv', 'a+') as f:
-                        f.write(str(imgUrl + "\n"))
+                    file_creator('_posted.tsv', 'a+', imgUrl + '\n')
                     with open(username + '_posted.tsv', 'r') as f:
                         last_line = str(f.readlines()[-1])
                         all_lines = str(f.readlines()[0:-2])
@@ -216,50 +249,46 @@ def instascraper(bot, new_media_id, path=POSTED_MEDIAS):
                         log.info("Image found in database scraping next profile")
                         x += 1
                         log.info("image of " + scraped_user)
-                        time.sleep(randint(5, 20))
+                        random_sleep(1, 2)
                         instascraper(bot, new_media_id, path=POSTED_MEDIAS)
 
             # Execute the repost function
-            time.sleep(randint(20, 40))
-            # Like Image
-            bot.api.like(media_id)
-            log.info("Liked media id: " + media_id)
-            time.sleep(randint(20, 50))
-            # Comment on Image
-            bot.comment(media_id, image_comment)
-            log.info("Commented: " + media_id)
-            time.sleep(randint(20, 40))
-            # Repost image
-            bot.api.upload_photo(instapath,tags)
-            log.info("Reposted: " + media_id)
+            random_sleep(1, 2)
             # Repost image as story
-          #  time.sleep(randint(20, 50))
-          #  bot.upload_story_photo(instapath)
-          #  log.info("Photo Uploaded to Story")
-            time.sleep(randint(20, 60))
+            log.info("Waiting")
+            random_sleep(100, 120)
+            bot.upload_story_photo(instapath)
+            log.info("Photo Uploaded to Story")
+            # Like Image
+            bot_like(media_id)
+            random_sleep(10, 25)
+            # Comment on Image
+            bot_comment(media_id, image_comment)
+            random_sleep(11, 26)
+            # Repost image
+            bot_upload_photo(instapath, tags, media_id)
+            random_sleep(2, 5)
             print(user_id)
             scraped_user_id = bot.get_user_id_from_username(scraped_user)
-            bot.send_message("hi i just reposted your photo", scraped_user_id)
-            log.info("Private dm send to " + scraped_user_id)
-            log.info("Wait 2200 - 2600 sec for next repost")
-            time.sleep(randint(3200, 3800))
+            send_dm(scraped_user_id)
+            random_sleep(3200, 3800)
         except:
             log.info("image set to private " + scraped_user)
             x += 1
-            time.sleep(randint(5, 20))
+            random_sleep(10, 22)
             instascraper(bot, new_media_id, path=POSTED_MEDIAS)
         x += 1
     x = 0
-    time.sleep(randint(10, 30))
+    random_sleep(5, 10)
     instascraper(bot, new_media_id, path=POSTED_MEDIAS)
 
 
 # All main stuff gets executed
 open_profiles()
-time.sleep(randint(5, 30))
+random_sleep(5, 10)
 bot = Bot()
 bot.login(username=args.u, password=args.p)
-time.sleep(randint(10, 30))
+random_sleep(5, 10)
 user_id = bot.get_user_id_from_username(args.u)
 username = bot.get_username_from_user_id(user_id)
 saveStats = bot.save_user_stats(username)
