96d173818ecaa661d2074979b420ded309a55ed6
Steffan Jensen <6hourapp@gmail.com>
2019-06-20 23:53:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='instabotai',
-    version='1.00.43',
+    version='1.00.44',
     description='Instagram bot scripts for promotion and API python wrapper.',
     long_description=long_description,
     long_description_content_type="text/markdown",
